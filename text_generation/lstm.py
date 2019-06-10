0c748a3563f2520f4d6af19970b154480d445792
tkoon107 <trevorlang@langdatascience.org>
2019-06-10 14:26:35 -0400

    new file

--- text_generation/lstm.py
+++ text_generation/lstm.py
@@ -15,18 +15,37 @@ text = text.lower()
 
 unique_chars =  sorted(list(set(text)))
 char_to_int = dict((c,i) for i, c in enumerate(unique_chars))
-n_chars = len(raw_text)
-n_vocab = len(chars)
+n_chars = len(text)
+n_vocab = len(unique_chars)
 
 SEQ_LENGTH = 100
 
 dataX = []
 dataY = []
 
-for i in range (0, n_charis - seq_length, 1):
-    seq_in = row_text[i: i + SEQ_LENGTH]
-    seq_out = row_text[i + SEQ_LENGTH]
+for i in range (0, n_chars - SEQ_LENGTH, 1):
+    seq_in = text[i: i + SEQ_LENGTH]
+    seq_out = text[i + SEQ_LENGTH]
     dataX.append([char_to_int[char] for char in seq_in])
     dataY.append(char_to_int[seq_out])
 
-n
+n_sequences = len(dataX)
+X = numpy.reshape(dataX, (n_sequences, SEQ_LENGTH, 1))
+X = X / float(n_vocab)
+
+y = np_utils.to_categorical(dataY)
+
+# define the LSTM model
+model = Sequential()
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
+model.add(Dropout(0.2))
+model.add(Dense(y.shape[1], activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+# define the checkpoint
+
+os.chdir("./model_checkpoints")
+filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list = [checkpoint]
+
